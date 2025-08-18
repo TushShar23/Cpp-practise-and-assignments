9188fcbf9ef6f8881c0d6a5bb21c9cd017857e61
Tushar Sharma <onlyforcode23@gmail.com>
2025-08-18 16:12:12 +0100

    add new programs files in Interesting C++ terminology folder

---
+++ INTRO/Interesting C++ terminology/Escape_seq_1.cpp
@@ -0,0 +1,9 @@
+//Using escape sequences to write statements
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "\\n is a new line character";
+    return 0;
+
+}
---
+++ INTRO/Interesting C++ terminology/Escape_seq_2.cpp
@@ -0,0 +1,9 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "C :\\MinGW\\mingw32\\lib\\ldscripts";
+    return 0;
+}
+// we have used double backslash coz one of them is escape sequence character.If we dont use it then it throws compilation error as backslash is an escape sequence and \M is not an escape seq character.So 
