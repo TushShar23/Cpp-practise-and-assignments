96006917020d867e6bfe48c69e0cafcec742a1a0
Tushar Sharma <onlyforcode23@gmail.com>
2025-08-22 13:26:58 +0100

    add files under If-else folder under programming construct folder

---
+++ PROGRAMMING CONSTRUCTS/If-else_switch_ternary/Mathematical_expr.cpp
@@ -0,0 +1,49 @@
+// Good question to confuse and a very good usecase of switchcase and single quotes.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    char s,q;
+    cin >> a >> s >> b >> q >> c ;
+
+   switch(s){
+    case '+' :
+        if(a + b == c){
+        cout << "Yes\n";
+        }
+        else{
+        cout << a + b << endl;
+        }
+         break;
+
+     case  '-' :
+        if(a-b==c){
+            cout << "Yes\n";
+        }
+        else{
+            cout << a-b << endl;
+        }
+        break;
+
+        case '*' :
+        if(a*b==c){
+            cout << "Yes\n";
+        }
+        else{
+            cout << a*b << endl;
+        }
+        break;
+
+        case '/':
+        if(a/b==c){
+            cout << "Yes\n";
+        }
+        else{
+            cout << a/b << endl;
+        }
+        break;
+   }
+
+}
